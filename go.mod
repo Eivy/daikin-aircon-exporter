2cb8f5f571ed756cc7d490c389c1228f23464e21
Eivy <modern.times.rock.and.roll+git@gmail.com>
2020-12-29 22:51:07 +0900

    Print version

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/Eivy/daikin-aircon-exporter
 
 go 1.15
-
-require github.com/prometheus/client_golang v1.9.0
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/eivy/daikin-aircon-exporter/exporter"
 )
@@ -9,8 +11,14 @@ import (
 func main() {
 	var listen string
 	var target string
+	var version bool
 	flag.StringVar(&listen, "listen address", "0.0.0.0:9823", "listen address")
 	flag.StringVar(&target, "target", "", "target aircon IP")
+	flag.BoolVar(&version, "version", false, "print version")
 	flag.Parse()
+	if version {
+		fmt.Println(Version)
+		os.Exit(0)
+	}
 	exporter.Run(listen, target)
 }
